ed407be912b28deebdbbd1100830f541ef2ccf59
Nontaphat Pongpis <folksvaken16@gmail.com>
2022-11-26 15:09:27 +0700

    Web React

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,10 +1,7 @@
 import Navbar from "../public/components/navbar";
 import Footer from "../public/components/Footer";
 import { Box, Heading, VStack } from "@chakra-ui/react";
-import CommonCard from "../public/components/Home/CommonCard";
-import RecipeCardAll from "../public/components/RecipeOfficial/RecipeCardAll";
-import NavPage from "../public/components/NavPage";
-import BlogHeader from "../public/components/RecipeOfficial/BlogHeader";
+
 import BlogDesc from "../public/components/RecipeOfficial/BlogDesc";
 
 export default function Recipe() {
